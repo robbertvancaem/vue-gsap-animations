c3d85cfcf3135c837a26b94416c5b0035e204b8c
Robbert van Caem <robbertvancaem@gmail.com>
2019-02-27 19:16:11 +0100

    Remove reference to svg-loader in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  chainWebpack: config => {
-    const svgRule = config.module.rule("svg");
-
-    svgRule.uses.clear();
-
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
-  },
   publicPath:
     process.env.NODE_ENV === "production" ? "/vue-gsap-animations/" : "/"
 };
