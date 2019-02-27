5c2e4d60658a874529955542c6ce8dd431d7df8a
Robbert van Caem <robbertvancaem@gmail.com>
2019-02-27 17:28:03 +0100

    Red box animation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Box />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Box from './components/box';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: {Box}
 }
 </script>
 
---
+++ src/components/box.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="box" class="box"></div>
+</template>
+
+<script>
+import { TimelineLite } from 'gsap';
+
+export default {
+  mounted() {
+    const { box } = this.$refs;
+    const timeline = new TimelineLite();
+
+    timeline.to(box, 1, { x: 200, rotation: 90 })
+  }
+}
+</script>
+
+<style>
+.box {
+  background: red;
+  height: 60px;
+  width: 60px;
+}
+</style>
